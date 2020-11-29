bba9f351f6d4b70491ac31cecdfde10e1f65f9e8
ilham <ilhamazalmad11@gmail.com>
2020-11-29 14:36:44 +0100

    Beans Update

--- AssuranceVie/src/main/java/com/example/AssuranceVie/dao/ClientDao.java
+++ AssuranceVie/src/main/java/com/example/AssuranceVie/dao/ClientDao.java
@@ -1,5 +1,11 @@
 package com.example.AssuranceVie.dao;
 
-public interface ClientDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.AssuranceVie.bean.Client;
+
+@Repository
+public interface ClientDao extends JpaRepository<Client,Long> {
 
 }
